6177cb59c5f0911a35ae9199744b9f61f2a5c97d
Tomasz Pasternak <pasternak.t@gmail.com>
2017-05-05 22:49:36 +0200

    Extracted function

--- main.hs
+++ main.hs
@@ -5,12 +5,11 @@ import Foreign.Ptr
 import Foreign.C.String
 import Foreign.Marshal.Alloc
 import Foreign.Storable
+import Data.Int
 
-data SIedConnection 
+data SIedConnection
 
 type IedClientError= CInt
-
--- 	IedConnection_connect (IedConnection self, IedClientError *error, const char *hostname, int tcpPort)
 
 foreign import ccall unsafe "iec61850_client.h IedConnection_connect"
   c_IedConnection_connect :: Ptr SIedConnection
@@ -22,11 +21,14 @@ foreign import ccall unsafe "iec61850_client.h IedConnection_connect"
 foreign import ccall unsafe "iec61850_client.h IedConnection_create"
   iedConnectionCreate :: IO(Ptr SIedConnection)
 
+iedConnectionConnect :: Ptr SIedConnection -> String -> Int32 -> IO IedClientError
+iedConnectionConnect con host port =
+  alloca $ \err -> do
+    useAsCString (pack "localhost") $ \str ->
+      c_IedConnection_connect con err str (CInt port)
+    peek err
 
 main = do
   con <-iedConnectionCreate
-  alloca $ \err -> do
-    useAsCString (pack "localhost") $ \str -> do
-      c_IedConnection_connect con err str (CInt 102)
-    e <- peek err
-    print e
+  e <- iedConnectionConnect con "hostname" 102
+  print e
