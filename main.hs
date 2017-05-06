b4ec52b7f6cce16b61a8ab19c58c2d364b935ebb
Tomasz Pasternak <pasternak.t@gmail.com>
2017-05-06 23:07:01 +0200

    Disconnect & destroy

--- main.hs
+++ main.hs
@@ -1,5 +1,5 @@
 import Data.Array
-import Data.ByteString.Char8
+import Data.ByteString.Char8 hiding (putStr)
 import Foreign.C.Types
 import Foreign.Ptr
 import Foreign.C.String
@@ -19,16 +19,41 @@ foreign import ccall unsafe "iec61850_client.h IedConnection_connect"
                              -> IO ()
 
 foreign import ccall unsafe "iec61850_client.h IedConnection_create"
-  iedConnectionCreate :: IO(Ptr SIedConnection)
+  c_IedConnectionCreate :: IO(Ptr SIedConnection)
+
+foreign import ccall unsafe "iec61850_client.h IedConnection_close"
+  c_IedConnection_close :: Ptr SIedConnection -> IO ()
+
+foreign import ccall unsafe "iec61850_client.h IedConnection_destroy"
+  c_IedConnection_destroy :: Ptr SIedConnection -> IO ()
 
 iedConnectionConnect :: Ptr SIedConnection -> String -> Int32 -> IO IedClientError
 iedConnectionConnect con host port =
   alloca $ \err -> do
-    useAsCString (pack "localhost") $ \str ->
+    useAsCString (pack host) $ \str ->
       c_IedConnection_connect con err str (CInt port)
     peek err
 
+connect :: String -> Int32 -> IO (Either IedClientError (Ptr SIedConnection))
+connect host port = do
+    con <- c_IedConnectionCreate
+    e <- iedConnectionConnect con host 102
+    case e of
+      0 -> return (Right con)
+      otherwise -> do
+        c_IedConnection_destroy con
+        return (Left e)
+
+destroy s = do
+  c_IedConnection_close s
+  c_IedConnection_destroy s
+
 main = do
-  con <-iedConnectionCreate
-  e <- iedConnectionConnect con "hostname" 102
-  print e
+  con <- connect "localhost" 102
+  case con of
+    Left e -> putStr "Failure: " >> print e
+    Right con -> do
+      destroy con
+      print "Success"
+
+
