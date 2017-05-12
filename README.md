ead6f3c069db8fb71295846c2878496b61933ba6
Tomasz Pasternak <pasternak.t@gmail.com>
2017-05-12 21:34:13 +0200

    Stack build system

---
+++ README.md
@@ -0,0 +1,1 @@
+# haskell61850-stack
--- convert.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-hsc2hs Enums.hsc -I ./libiec61850-1.0.1/src/iec61850/inc/ -I libiec61850-1.0.1/src/common/inc/ -Ilibiec61850-1.0.1/src/hal/inc/ -Ilibiec61850-1.0.1/src/mms/inc/ -Ilibiec61850-1.0.1/src/logging/
