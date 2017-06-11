d58da4188e71b7d177e97911d09e666a424c8505
Tomasz Pasternak <pasternak.t@gmail.com>
2017-06-11 19:06:48 +0200

    New model discovery method

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,8 @@
 import           Test.HUnit
 
-import           BitString
+import           Data.List
+import           Data.List.Split
+import           Iec61850.BitString
 
 main = runTestTT tests
 
@@ -17,4 +19,43 @@ testShow =
       actual = show $ BitString 4
   in TestCase $ assertEqual "" expected actual
 
-tests = TestList [TestLabel "testShow" testShow, TestLabel "testToList" testToList]
+tests = TestList
+          [ TestLabel "testShow" testShow
+          , TestLabel "testToList" testToList
+          , TestLabel "buildNameTree1" buildNameTree1
+          , TestLabel "buildNameTree2" buildNameTree2
+          , TestLabel "buildNameTree3" buildNameTree3
+          , TestLabel "buildNameTree4" buildNameTree4
+          ]
+
+buildNameTree1 = TestCase $ assertEqual "" [] (buildNameTree [])
+
+buildNameTree2 = TestCase $ assertEqual "" [Leaf "MX"] (buildNameTree ["MX"])
+
+buildNameTree3 = TestCase $ assertEqual "" [Node "MX" [Leaf "A1"]] (buildNameTree ["MX", "MX$A1"])
+
+buildNameTree4 = TestCase $ assertEqual "" [(Node "MX" [Leaf "A1"]), Leaf "CX"]
+                              (buildNameTree ["MX", "MX$A1", "CX"])
+
+data DiscoverStruct = Leaf String
+                    | Node String [DiscoverStruct]
+  deriving (Eq, Show)
+
+buildNameTreeH :: [[String]] -> [DiscoverStruct]
+buildNameTreeH split_ =
+  let roots = concat $ filter ((== 1) . length) split_
+      newTrees = map
+                   (\root -> let nextString = filter (\tree -> length tree >= 1 && root == head tree) split_
+                                 asn = if length nextString == 1
+                                         then Leaf root
+                                         else Node root (buildNameTreeH (drop 1 <$> nextString))
+                             in asn)
+                   roots
+  in newTrees
+
+buildNameTree :: [String] -> [DiscoverStruct]
+buildNameTree strings =
+  let split_ = map (splitOn "$") strings
+      s = buildNameTreeH split_
+  in s--
+      -- ["ST$Proxy$t","ST$Proxy$stVal","ST$Proxy$q","ST$Proxy","ST$PhyHealth$t","ST$PhyHealth$stVal","ST$PhyHealth$q","ST$PhyHealth","ST","DC$PhyNam$vendor","DC$PhyNam","DC"]
